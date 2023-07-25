5c773644f43da7cd2fa60032df8c2c2abe4f0707
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-07-25 17:27:07 -0400

    Feat: new files and new features
    It was created new files such as; Header.jsx, header.MUI.styles.js. Also was implemented Header.jsx in Landing.jsx

--- tony-shop/src/atoms/landing/Landing.jsx
+++ tony-shop/src/atoms/landing/Landing.jsx
@@ -1,8 +1,11 @@
 import '@atoms/landing/Landing.scss';
+// import Header from '../../molecules/header/Header';
+import Header from '@molecules/header/Header';
 
 const Landing = () => {
   return (
     <div>
+      <Header />
       <h1 className="prueba"> SOY EL LANDING PAGE</h1>
     </div>
   );
