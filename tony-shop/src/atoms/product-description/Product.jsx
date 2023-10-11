a8de692e906414ad2d124401a8703c1610041623
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-10-11 15:54:31 -0400

    Feat: new feature at Product.jsx
    It were added new features to product images in the component Product.jsx and its scss file

--- tony-shop/src/atoms/product-description/Product.jsx
+++ tony-shop/src/atoms/product-description/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Container from '@mui/material/Container';
@@ -19,18 +19,72 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Favorite from '@mui/icons-material/Favorite';
 import Stack from '@mui/material/Stack';
 
+// cosas para la img
+import AspectRatio from '@mui/joy/AspectRatio';
+import Avatar from '@mui/joy/Avatar';
+
 // css
 import '../product-description/Product.scss';
-import { grey } from '@mui/material/colors';
 
 const Product = () => {
   const { idProduct } = useParams();
+
+  const imgPrueba = {
+    img1: 'https://edgehelmets.com/cdn/shop/files/CAPITAN_AMERICA_ZOOM.jpg?v=1687202275',
+    img2: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8vW2ZGX96EGYANmWC0lf5hMP15jAGuV-K9fb0GQoUhnwLNOybsrAuJBRp_YpPQNzP6Pk&usqp=CAU',
+    img3: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO7xhvZAEtfZBmxQGVGHWGJRWBp3I0sFV3SYPn9zqjQofUerr6UYaFBQsyQ_N_lqWt3Z0&usqp=CAU',
+    img4: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXSyB0T6VoY4C2hudY6QUdpQTJVt2ppuCyjomDNqUJf20x3SrvQMxzU_FG8jQKdMFGr3E&usqp=CAU'
+  };
+
+  const [ImgProduct, setImgProduct] = useState(imgPrueba.img1);
+
+  const showImgProduct = (img) => {
+    setImgProduct(img);
+  };
 
   return (
     <div className="bg-container-grid">
       <Container fixed>
         <div className="container-prueba ">
-          <div className="img-product">div1</div>
+          <div className="img-product">
+            <AspectRatio objectFit="contain">
+              <img srcSet={ImgProduct} alt="image of product" />
+            </AspectRatio>
+            <Stack direction={'row'} justifyContent="center" alignItems="center" gap={2} sx={{ marginTop: '.5rem' }}>
+              <Avatar
+                alt="Remy Sharp"
+                variant="outlined"
+                src={imgPrueba.img1}
+                size="lg"
+                sx={{ cursor: 'pointer', '&:hover': { border: '1px solid var(--blue)' } }}
+                onClick={() => showImgProduct(imgPrueba.img1)}
+              />
+              <Avatar
+                alt="Remy Sharp"
+                variant="outlined"
+                src={imgPrueba.img2}
+                size="lg"
+                sx={{ cursor: 'pointer', '&:hover': { border: '1px solid var(--blue)' } }}
+                onClick={() => showImgProduct(imgPrueba.img2)}
+              />
+              <Avatar
+                alt="Remy Sharp"
+                variant="outlined"
+                src={imgPrueba.img3}
+                size="lg"
+                sx={{ cursor: 'pointer', '&:hover': { border: '1px solid var(--blue)' } }}
+                onClick={() => showImgProduct(imgPrueba.img3)}
+              />
+              <Avatar
+                alt="Remy Sharp"
+                variant="outlined"
+                src={imgPrueba.img4}
+                size="lg"
+                sx={{ cursor: 'pointer', '&:hover': { border: '1px solid var(--blue)' } }}
+                onClick={() => showImgProduct(imgPrueba.img4)}
+              />
+            </Stack>
+          </div>
           <div className="menu">
             <Card variant="outline" color="neutral" sx={{ height: 'auto', width: '100%' }}>
               <Stack
