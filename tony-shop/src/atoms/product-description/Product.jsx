58bd097f6c993b1b396cf07b6960e338b74ed265
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-10-12 20:11:36 -0400

    Feat: new feature
    It were added new features to info section in Product.jsx

--- tony-shop/src/atoms/product-description/Product.jsx
+++ tony-shop/src/atoms/product-description/Product.jsx
@@ -23,6 +23,13 @@ import Stack from '@mui/material/Stack';
 import AspectRatio from '@mui/joy/AspectRatio';
 import Avatar from '@mui/joy/Avatar';
 
+// cosas para la info
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Divider from '@mui/joy/Divider';
+
 // css
 import '../product-description/Product.scss';
 
@@ -201,9 +208,50 @@ const Product = () => {
               </CardActions>
             </Card>
           </div>
-          <div className="info">div3</div>
-          <div className="pregunta">div4</div>
-          <div className="calificacion">div5</div>
+          <div className="info">
+            <Accordion defaultExpanded={true}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ fontSize: '20px' }}>
+                Informacion del producto
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="subtitle1">Caracteristicas</Typography>
+                {/* <Divider /> */}
+                <Typography variant="body2" sx={{ marginBottom: '1rem' }}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam rem molestiae iste dolorum quo rerum
+                  velit quibusdam harum perspiciatis omnis. Aspernatur odit quam ad iste quibusdam beatae et, ipsa sint!
+                </Typography>
+
+                <Divider sx={{ margin: '2rem 0rem' }} />
+
+                <Typography variant="body1">Descripcion</Typography>
+                {/* <Divider /> */}
+                <Typography variant="body2" align="justify">
+                  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                  classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
+                  professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
+                  consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
+                  literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
+                  de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book
+                  is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem
+                  Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem
+                  Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
+                  de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied
+                  by English versions from the 1914 translation by H. Rackham.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            {/* <Accordion defaultExpanded={true}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>Descripcion</AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam rem molestiae iste dolorum quo rerum
+                  velit quibusdam harum perspiciatis omnis. Aspernatur odit quam ad iste quibusdam beatae et, ipsa sint!
+                </Typography>
+              </AccordionDetails>
+            </Accordion> */}
+          </div>
+          <div className="pregunta">preguntas</div>
+          <div className="calificacion">calificaciones</div>
         </div>
       </Container>
     </div>
