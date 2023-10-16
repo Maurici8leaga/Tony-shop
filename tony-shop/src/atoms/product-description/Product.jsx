e403cc9dda03a901e3c28943cdc42e2b095dd752
Mauricio Oleaga <maoa.develoment@gmail.com>
2023-10-16 15:21:59 -0400

    Feat: new feature
    It were added  question & answer section to Product.jsx

--- tony-shop/src/atoms/product-description/Product.jsx
+++ tony-shop/src/atoms/product-description/Product.jsx
@@ -34,6 +34,13 @@ import Divider from '@mui/joy/Divider';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+
+// cosass para las preguntas
+import TextField from '@mui/material/TextField';
+import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
+
 // css
 import '../product-description/Product.scss';
 
@@ -290,7 +297,68 @@ const Product = () => {
               </AccordionDetails>
             </Accordion>
           </div>
-          <div className="pregunta">preguntas</div>
+          <div className="pregunta">
+            <Accordion defaultExpanded={true}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ fontSize: '20px' }}>
+                Preguntas y respuestas
+              </AccordionSummary>
+              <AccordionDetails sx={{ width: '700px', margin: 'auto' }}>
+                <Box
+                  component="form"
+                  direction={'row'}
+                  sx={{ marginY: 2, width: '100%', display: 'flex', justifyContent: 'space-evenly' }}
+                >
+                  <TextField
+                    id="input-question"
+                    // label="Make Question"
+                    value=""
+                    variant="outlined"
+                    type="text"
+                    placeholder="Haz una pregunta"
+                    sx={{ width: '70%' }}
+                  />
+
+                  <Button type="submit" variant="outlined" size="md" width={'30%'}>
+                    Preguntar
+                  </Button>
+                </Box>
+
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    width: '500px',
+                    height: 'auto',
+                    margin: 'auto'
+                  }}
+                >
+                  <List>
+                    {/* pregunta */}
+                    <ListItem>
+                      <ListItemText>
+                        <Typography variant="body2">
+                          Lorem ipsum dolor sit amet consectetur, adipisicing elit?
+                        </Typography>
+                      </ListItemText>
+                    </ListItem>
+                    {/* respuesta */}
+                    <ListItem>
+                      <ListItemIcon>
+                        <SubdirectoryArrowRightIcon />
+                      </ListItemIcon>
+                      <ListItemText>
+                        <Typography variant="body2" sx={{ fontWeight: 'lighter' }}>
+                          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi quaerat laboriosam
+                          similique modi voluptates voluptate ut aperiam provident
+                        </Typography>
+                      </ListItemText>
+                    </ListItem>
+                  </List>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          </div>
           <div className="calificacion">calificaciones</div>
         </div>
       </Container>
