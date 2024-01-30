4d194ed54a30d61346b6c9694df6b00e108d82a6
Mauricio Oleaga <maoa.develoment@gmail.com>
2024-01-30 19:17:12 -0400

    Feat: new dependency, files and features
    It was installed json-server and implemented in the file Api/db.json . Also was created the reducer user.reducer in the folder redux-toolkit/reducers/user . Finallywas implemented the user.reducer in the file store.js

---
+++ src/redux-toolkit/reducers/user/user.reducer.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+// createSlice permite definir las acciones disponibles para interacturar con los datos del estado inicial de un reducer particular
+
+// con REDUX-TOOLKIT los actions van ahora aqui adentro de los Slices
+
+// estos actions se repetiran por los modelos que tengas user, carrito etc. En este caso es referente  al user
+
+// de esta forma se declara el estado inicial de  las  propiedades que tendra este reducer
+const initialState = {
+  token: '',
+  profile: null
+};
+
+const userSlice = createSlice({
+  name: 'user', // nombre del reducer que se reconocera entre los otros que existan
+  initialState,
+  reducers: {
+    // aqui ira la logica para modificar el state del objeto que este en el store
+    addUser: (state, action) => {
+      const { token, profile } = action.payload;
+      state.token = token;
+      state.profile = profile;
+    }
+  }
+});
+
+// para exportar las actions
+export const { addUser } = userSlice;
+// para exportar el reducer
+export default userSlice.reducer;
--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './reducers/user/user.reducer';
 
 // este viene siendo como el index de los reducers, donde se declaran los reducers existentes
 export const store = configureStore({
   // es necesario como minimo colocar "reducer: {}" porque si no Provider no va a renderizar los components
-  reducer: {}
+  reducer: {
+    user: userReducer
+  }
 });
